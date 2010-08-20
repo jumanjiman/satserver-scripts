3101b03c942fb4ab73489fd91ead1954962c0203
Paul Morgan <jumanjiman@gmail.com>
2010-08-20 10:48:12 -0400

    sample output

--- spacecmd-fu/src/list-dupe-profiles.sh
+++ spacecmd-fu/src/list-dupe-profiles.sh
@@ -5,3 +5,12 @@
 for i in $(spacecmd system_list); do
   spacecmd system_details $i | grep WARNING
 done
+
+## -- Returns output like:
+# WARNING: Multiple systems found with the same name
+# WARNING: id-rhds01 = 1000017547
+# WARNING: id-rhds01 = 1000017567
+# WARNING: id-rhds01 = 1000017587
+# WARNING: Multiple systems found with the same name
+# WARNING: ob-gts01.test.ise.com = 1000014690
+# WARNING: ob-gts01.test.ise.com = 1000017467
